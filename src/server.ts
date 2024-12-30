f106bc4b2b4a9d4ff0cf061656e0a1c5a24a1cf6
OriAbulafia <abulafiaori@gmail.com>
2024-12-30 14:18:55 +0200

    First commit

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express, { Express } from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const initApp = (): Promise<Express> => {
+  return new Promise<Express>((resolve, reject) => {
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+    db.once("open", function () {
+      console.log("Connected to the database");
+    });
+    if (!process.env.DB_CONNECT) {
+      reject("DB_CONNECT is not defined");
+    } else {
+      mongoose
+        .connect(process.env.DB_CONNECT)
+        .then(() => {
+          resolve(app);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }
+  });
+};
+
+export default initApp;
